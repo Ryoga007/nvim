2f8bf4771abfa9d8821bfe9c96b230987c7f9d55
pepe <Ryogahp@hotmail.com>
2023-10-11 11:49:19 -0400

    agrgando keymaps y cmd a fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	cmd = {"G", "Git"},
+	Keys = {
+		{"<leader>ga", ":Git fetch ---all -p<cr>", desc="Git fetch"},
+		{"<leader>gl", ":Git pull<cr>", desc="Git pull"},
+	},
 }
